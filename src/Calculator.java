05bd3c249ba898f2844ab9720da8daddf2232e3c
LacicMarijo <marijo.lacic@gmail.com>
2021-10-08 19:59:09 +0200

    Variablen erstellt

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,16 @@
 public class Calculator {
     public static void main(String[] args) {
+    int a = 1;
+    double b = 2.0;
+    boolean c = true;
+    char d = 'c';
+    String e = "Hey";
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+
 
     }
 }
